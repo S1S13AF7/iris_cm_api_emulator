824a0413c1dfddc80bc5eae12d71b149b53b4404
S1S13AF7 <S1S13AF7@MISC.PP.UA>
2021-02-17 15:50:46 +0200

    Update set_id.py

--- iris_cm_api/handlers/set_id.py
+++ iris_cm_api/handlers/set_id.py
@@ -9,8 +9,10 @@ bot.labeler.vbml_ignore_case = True
 @bot.on.chat_message(text="установить ид <chat_id>")
 @utils.admin_only
 async def set_iris_id_wrapper(m: Message, chat_id: str):
-    if len(chat_id) > 10:
+    if len(chat_id) > 8:
         return "⚠ Слишком большой ID"
+    if len(chat_id) < 8:
+        return "⚠ Слишком короткий ID"
 
     chat = await utils.get_chat(m.peer_id)
     chat.iris_id = chat_id
